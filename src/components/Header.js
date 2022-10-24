1f624621ee449afe1c006416a9da41c1c30a1a4b
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-10-24 11:07:21 +0500

    Refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
-import headerStyles from "../styles/header.module.scss";
+import styles from "../styles/header.module.scss";
 
 const Header = (props) => {
-  return <div className={headerStyles.header}>{props.children}</div>;
+  return <div className={styles.header}>{props.children}</div>;
 };
 
 export default Header;
