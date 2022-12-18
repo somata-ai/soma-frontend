bfb7c9b759646e72ef3d10cd373fd01b096e58af
Hasan-Iqtedar <hasan.iqtedar@gmail.com>
2022-12-18 14:06:27 +0500

    Add add/remove buttons

--- src/components/Layer.js
+++ src/components/Layer.js
@@ -1,5 +1,9 @@
-import styles from "../styles/layer.module.scss";
+import {
+  IoIosAddCircleOutline,
+  IoIosRemoveCircleOutline,
+} from "react-icons/io";
 import Neuron from "./Neuron";
+import styles from "../styles/layer.module.scss";
 
 const Layer = (props) => {
   return (
@@ -7,6 +11,10 @@ const Layer = (props) => {
       {[...Array(props.layer.neurons)].map((x) => {
         return <Neuron />;
       })}
+      <div className={styles.buttonsContainer}>
+        <IoIosAddCircleOutline className={styles.button} />
+        <IoIosRemoveCircleOutline className={styles.button} />
+      </div>
     </div>
   );
 };
