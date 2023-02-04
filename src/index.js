262e2c5683d34ebc9880f00b916937bcec17eea6
Hasan-Iqtedar <hasan.iqtedar@gmail.com>
2023-02-04 21:21:22 +0500

    Set up router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-    <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signUp" element={<SignUpPage />} />
+    </Routes>
+  </BrowserRouter>
   // </React.StrictMode>
 );
