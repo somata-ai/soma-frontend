d6ac9c702e6cbe04cabf76331d0aabb2688bcdd8
Hasan-Iqtedar <hasan.iqtedar@gmail.com>
2023-02-04 21:31:54 +0500

    Add ProtectedRoute component

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,14 @@
+import { useAuth } from "../contexts/auth";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = (props) => {
+  const auth = useAuth();
+
+  if (!auth.user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return props.children;
+};
+
+export default ProtectedRoute;
