e1452865552471d0b4dc576ffdf3c4fc44714c48
Hasan-Iqtedar <hasan.iqtedar@gmail.com>
2023-02-04 21:41:42 +0500

    Add Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,30 @@
-import styles from "../styles/header.module.css";
+import { NavLink } from "react-router-dom";
+import { useAuth } from "../context/auth";
 
 const Header = (props) => {
-  return <div className={styles.header}>{props.children}</div>;
+  const auth = useAuth();
+
+  return (
+    <div className="bg-purple-900 h-12 flex flex-row items-center">
+      <h1 className="text-white text-2xl ml-2">Soma</h1>
+      {auth.user ? (
+        <div>Welcome</div>
+      ) : (
+        <div className="flex justify-evenly w-1/6 ml-auto mr-5">
+          <NavLink to={"/login"}>
+            <div className="text-white transition ease-in hover:scale-110 duration-200">
+              Login
+            </div>
+          </NavLink>
+          <NavLink to={"/login"}>
+            <div className="text-white transition ease-in hover:scale-110 duration-200">
+              SignUp
+            </div>
+          </NavLink>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Header;
