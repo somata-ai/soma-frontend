ba94dccea417b36e501b9936e59b183dc4d7ee62
Talha Munir <talhamunir411@gmail.com>
2023-02-05 21:36:09 +0500

    Added the signup page

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,5 +1,46 @@
+import React from "react";
+import { useState } from "react";
+import Header from "../components/Header";
+
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
-};
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleSubmit = event => {
+
+    if (password !== confirmPassword) {
+      setError("Both the passwords do not match")
+    }
+    console.log(email, password);
+    console.log(event.state);
+  };
+
+
+  return (
+    <div>
+      <Header />
+      <div className="bg-slate-50 container mx-auto my-auto mt-20 py-6 w-96 h-4/6 shadow-lg rounded-md p-1">
+        <form className="grid justify-center" onSubmit={handleSubmit}>
+          
+          <h1 className="text-4xl text-purple-500 mx-auto mb-5">SignUp for SOMA</h1>
+          <label className="text-[#a454fc] mb-1 ml-4">Username</label>
+          <input className="rounded-md p-1 shadow-md w-64 h-8 mx-auto" type="email" placeholder="e.g talhahahae" value={email} onChange={e => setEmail(e.target.value)} />
+          <br />
+          <label className="text-[#a454fc] mb-1 ml-4">Password</label>
+          <input className="rounded-md p-1 shadow-md w-64 h-8 mx-auto" type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+          <br />
+          <label className="text-[#a454fc] mb-1 ml-4">Confirm Password</label>
+          <input className="rounded-md p-1 shadow-md w-64 h-8 mx-auto" type="confirmPassword" placeholder="Confirm Password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
+          <br />
+          <button className="bg-[#a454fc] font-sans text-2xl text-white rounded-lg w-80 h-12 mb-5" type="submit" onSubmit={() => handleSubmit()}>Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
 
 export default SignUpPage;
