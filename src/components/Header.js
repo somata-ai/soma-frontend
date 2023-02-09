77d065dbcae6be83023ddf1c0ba76f89b8b5b0c8
Hasan-Iqtedar <hasan.iqtedar@gmail.com>
2023-02-09 20:55:44 +0500

    Add content for logged in user.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,30 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/auth";
 
 const Header = (props) => {
   const auth = useAuth();
+  const navigate = useNavigate();
 
   return (
     <div className="bg-purple-900 h-12 flex flex-row items-center">
       <h1 className="text-white text-2xl ml-2">Soma</h1>
       {auth.user ? (
-        <div>Welcome</div>
+        <div className="flex justify-evenly w-1/6 ml-auto mr-5">
+          <NavLink to={"/model"}>
+            <div className="text-white transition ease-in hover:scale-110 duration-200">
+              Create
+            </div>
+          </NavLink>
+          <div
+            className="text-white transition ease-in hover:cursor-pointer hover:scale-110 duration-200"
+            onClick={(e) => {
+              auth.logout();
+              navigate("/login", { replace: true });
+            }}
+          >
+            Logout
+          </div>
+        </div>
       ) : (
         <div className="flex justify-evenly w-1/6 ml-auto mr-5">
           <NavLink to={"/login"}>
