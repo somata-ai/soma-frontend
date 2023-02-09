569b733006dc64cfeee218bba63cafcee4d04ac6
Hasan-Iqtedar <hasan.iqtedar@gmail.com>
2023-02-09 23:10:30 +0500

    Add protected routes.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import * as tf from "@tensorflow/tfjs";
 import { useEffect } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Header from "./components/Header";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Dashboard from "./pages/Dashboard";
 import Network from "./pages/Network";
 import Settings from "./pages/Settings";
@@ -22,8 +23,22 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/model" replace />} />
         <Route path="/model" element={<Network />} />
-        <Route path="/profile" element={<Dashboard />} />
-        <Route path="/settings" element={<Settings />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <ProtectedRoute>
+              <Settings />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </div>
   );
